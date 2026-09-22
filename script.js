50f609621e610f743e73e675045bb78df6545aa1
saikumar <sai06.bammidi@gmail.com>
2026-09-22 13:01:59 +0530

    learn how call backs works

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ setTimeout(function () {
   console.log("prints after 2 sec")
 },2000)
 
+document.getElementById("btn").addEventListener("click", function () {
+  console.log("button clicked")
+})
 console.log("hellowwww")
