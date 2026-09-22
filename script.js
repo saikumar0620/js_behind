0f9826445c99b8db6814c750f80cf02b5e34b81b
saikumar <sai06.bammidi@gmail.com>
2026-09-22 13:17:51 +0530

    refactor: update fetch logic and clean up commented code

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ setTimeout(function () {
   console.log("prints after 2 sec")
 },2000)
 
-document.getElementById("btn").addEventListener("click", function () {
-  console.log("button clicked")
-})
+
+fetch('https://typicode.com')
+try {
+  console.log("data has been fetched ")
+  
+} catch (error) {
+  console.error()
+}
+
+// document.getElementById("btn").addEventListener("click", function () {
+//   console.log("button clicked")
+// })
 console.log("hellowwww")
