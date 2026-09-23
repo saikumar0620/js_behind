19ae1a5a70343345f7d6cef2e7abed1c8693e877
saikumar <sai06.bammidi@gmail.com>
2026-09-23 16:17:09 +0530

    refactor: clean up code by commenting out unused sections and adding area calculation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("hello world")
+
 setTimeout(function () {
   console.log("prints after 2 sec")
 },2000)
@@ -15,10 +17,26 @@ document.getElementById("btn").addEventListener("click", function () {
   console.log("button clicked")
 })
 
-let startDate = new Date().getTime()
-let endDate = startDate;
-while (endDate < startDate + 10000) {
-  endDate = new Date().getTime()
-}
-console.log("10 sec has passed")
+// let startDate = new Date().getTime()
+// let endDate = startDate;
+// while (endDate < startDate + 10000) {
+//   endDate = new Date().getTime()
+// }
+// console.log("10 sec has passed")
 
+
+const radius = [3, 1, 2, 4]
+const area = function (radius) {
+  return Math.PI * radius * radius;   
+}
+
+
+const calculateArea = function (radius, logic) {
+  const output = []
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]))
+  }
+  return output;
+}
+let result = calculateArea(radius, area)
+console.log(result)
