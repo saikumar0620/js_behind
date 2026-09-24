f85e29059098bd0fd82ff8d0404c46c2a4060ff7
saikumar <sai06.bammidi@gmail.com>
2026-09-24 14:15:44 +0530

    recape logic

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-console.log("hello world")
-
-setTimeout(function () {
-  console.log("prints after 2 sec")
-},2000)
-
 
 // fetch('https://typicode.com')
 // try {
@@ -13,9 +7,9 @@ setTimeout(function () {
 //   console.error()
 // }
 
-document.getElementById("btn").addEventListener("click", function () {
-  console.log("button clicked")
-})
+// document.getElementById("btn").addEventListener("click", function () {
+//   console.log("button clicked")
+// })
 
 // let startDate = new Date().getTime()
 // let endDate = startDate;
@@ -25,18 +19,50 @@ document.getElementById("btn").addEventListener("click", function () {
 // console.log("10 sec has passed")
 
 
-const radius = [3, 1, 2, 4]
-const area = function (radius) {
-  return Math.PI * radius * radius;   
+// const radius = [3, 1, 2, 4]
+// const area = function (radius) {
+//   return Math.PI * radius * radius;   
+// }
+
+// console.log(radius.map(area))
+
+// const calculateArea = function (radius, logic) {
+//   const output = []
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(logic(radius[i]))
+//   }
+//   return output;
+// }
+// let result = calculateArea(radius, area)
+// console.log(result)
+
+
+// const radiuss = [3, 2, 1, 4]
+// const areaa = function (radiuss) {
+//   return Math.PI*radiuss*radiuss
+// }
+
+// const calculates = function (radiuss,formula) {
+//   let output = []
+//   for (i = 0; i < radiuss.length; i++){
+//     output.push(formula(radiuss[i]))
+//   }
+//   return output
+// }
+// let res = calculates(radiuss, areaa)
+// console.log(res)
+
+const radius = [2, 1, 3, 4]
+const logic = function (radius) {
+  return Math.PI * radius * radius
 }
 
-
-const calculateArea = function (radius, logic) {
-  const output = []
-  for (let i = 0; i < radius.length; i++) {
+const cal = function (radius,logic) {
+  let output=[]
+  for ( i = 0; i < radius.length; i++){
     output.push(logic(radius[i]))
   }
   return output;
 }
-let result = calculateArea(radius, area)
+let result = cal(radius,logic)
 console.log(result)
